2cd45a28962a6e6cff7788dc5ad4c046d440e8a1
Dan DeFelippi <dan@driverdan.com>
2010-11-05 18:22:45 -0400

    Moved all inline JS into master.js

--- master.js
+++ master.js
@@ -582,6 +582,243 @@ function timerFix() {
 timerFix();
 
 
+/** TAKEN FROM INDEX **/
+var nbBanner = 3
+	,idSuivant
+	,puce
+	,monTimer;
+
+function afficheBanner(idSolde, rotationSolde) {
+	var divImgCible;
+	puce = parseInt(idSolde.substring(12));
+	idSuivant = puce + 1;
+	if (idSuivant >= nbBanner) idSuivant = 0;
+	var spanPuceCible = document.getElementById("puce_" + puce);
+	for (var i = 0; i < nbBanner; i++) {
+		var allDivs = document.getElementById("visu_banner_" + i);
+		allDivs.style.display = "none";
+		divImgCible = document.getElementById(idSolde);
+		divImgCible.style.display = "block";
+		var allPuces = document.getElementById("puce_" + i);
+		allPuces.className = allPuces.className.replace(/active/, "");
+		if (!/active/.test(spanPuceCible.className)) spanPuceCible.className += " active";
+	}
+	if (rotationSolde == "rotationOk") {
+		monTimer = setTimeout("afficheBanner('visu_banner_'+idSuivant,'rotationOk')", 3500);
+	}
+	else {
+		clearTimeout(monTimer);
+		monTimer = setTimeout("afficheBanner('visu_banner_'+idSuivant,'rotationOk')", 10000);
+	}
+}
+/** END **/
+
+
+/** PLAYER CODE TAKEN FROM INDEX **/
+
+//-------------------------------//
+//	Management of the player 30s
+//-------------------------------//
+var StreamPlayer = null;
+var NewStreamPlayer = false;
+var onPlayFlag = false; //etat du player:lecture/arret
+var IsOpen = false; //etat du player:fermer/ouvert
+//Mise a jour de l"etat du player depuis les cookies
+var temp = GetPlayerCookie("PlayerPlaying");
+var lRootNid = -57;
+var vnDisques = -2;
+var vnFnacMusic = -70;
+
+switch (temp) {
+case '1':
+	onPlayFlag = true;
+	break;
+case '0':
+	onPlayFlag = false;
+	break;
+}
+
+temp = GetPlayerCookie("Player");
+
+switch (temp) {
+case '1':
+	IsOpen = true;
+	break;
+case '0':
+	IsOpen = false;
+	break;
+}
+//It forces the creation of the player to be able to properly update the value of cookies
+if ((lRootNid == vnDisques) || (lRootNid == vnFnacMusic)) {
+	if (IsOpen) StreamPlayer = getStreamPlayerWindow();
+}
+
+var ParamAddProduct = null;
+var playing = false;
+var typePlaying = '';
+
+function getStreamPlayerWindow() {
+	var wwwNetRootUrl = 'http://www4.fnac.com';
+	try {
+		NewStreamPlayer = false;
+		var widthscreen = screen.Width;
+		var widthplayer = 997;
+		var heightscreen = screen.Height;
+		var heightplayer = 520;
+		var posleft = parseInt((screen.availWidth / 2) - (widthplayer / 2)) + 300;
+		var postop = parseInt((screen.availHeight / 2) - (heightplayer / 2));
+		var playerWindow = window.open("", "Player30sec", "menubar=no,directories=no,location=no,resizable=yes,titlebar=no,scrollbars=no,status=no,toolbar=no,height=1px,width=1px,top=" + postop + ",left=" + posleft + "");
+
+		var wwwNetRootUrl = 'http://www4.fnac.com';
+		if (playerWindow == null) {
+			return null;
+		}
+		try {
+			if (playerWindow.document.getElementById("divPlayer") == null) {
+				NewStreamPlayer = true;
+				playerWindow.location.href = wwwNetRootUrl + "/shelf/common/content/Player30s.aspx";
+			}
+		}
+		catch (e) {
+			NewStreamPlayer = true;
+			playerWindow.location.href = wwwNetRootUrl + "/shelf/common/content/Player30s.aspx";
+		}
+
+		return playerWindow;
+	}
+	catch (e) {
+		return null;
+	}
+}
+
+function AddProductPointer() {
+	if (ParamAddProduct && StreamPlayer) {
+		StreamPlayer.AddProduct(ParamAddProduct, playing);
+		ParamAddProduct = null;
+	}
+}
+
+function AddProduct(id, type) {
+	playing = false;
+	typePlaying = type;
+	StreamPlayer = getStreamPlayerWindow();
+
+	if (StreamPlayer != null && id != null && id != '') {
+		ParamAddProduct = id;
+		if (NewStreamPlayer) {
+			//new popup
+			playing = true;
+			setTimeout(AddProductPointer, 3000);
+		}
+		else {
+			AddProductPointer();
+		}
+	}
+	var isChrome = /chrome/.test(navigator.userAgent.toLowerCase());
+	if (isChrome) {
+		StreamPlayer.blur();
+	}
+	StreamPlayer.focus();
+}
+
+//Play / Stop button can manage the exclusive read between the reader and the reader 30s heart page
+function DoPlay(flag) {
+	try {
+		if (StreamPlayer) {
+			StreamPlayer.DoPlay(flag);
+		} else {
+			//If the reader has been opened in a previous page
+			if (IsOpen) {
+				StreamPlayer = getStreamPlayerWindow();
+				if (StreamPlayer) {
+					StreamPlayer.DoPlay(flag);
+				}
+			}
+		}
+	}
+	catch (e) {}
+}
+
+//return on the state of the player
+function Onplay(flag) {
+	// It saves the state of the player 30s
+	onPlayFlag = flag;
+	//If the player has passed the state reading it stops the heart player page
+	if (onPlayFlag == true) {
+		try {
+			MultiAudioPlayer.stopCurrentPlayer();
+		}
+		catch (e) {}
+	}
+}
+//Back when adding to the player
+
+function OnItemAdded(ids) {
+
+}
+
+function PlayThisMedia(eventArgs, who) {
+	Onplay();
+}
+//----//
+//	Navigation management
+//----//
+//Transition to another page it saves the state of the player in cookies
+window.onbeforeunload = updatePlayerCookies;
+//----//
+//	Manage Cookies
+//----//
+
+//Reading Cookies
+function GetPlayerCookie(name) {
+	var arg = name + "=";
+	var alen = arg.length;
+	var clen = document.cookie.length;
+	var i = 0;
+	while (i < clen) {
+		var j = i + alen;
+		if (document.cookie.substring(i, j) == arg) return getPlayerCookieVal(j);
+		i = document.cookie.indexOf(" ", i) + 1;
+		if (i == 0) break;
+	}
+	return -1;
+}
+
+function getPlayerCookieVal(offset) {
+	var endstr = document.cookie.indexOf(";", offset);
+	if (endstr == -1) endstr = document.cookie.length;
+	return unescape(document.cookie.substring(offset, endstr));
+}
+
+//Update cookies
+function updatePlayerCookies() {
+	var Player = GetPlayerCookie("Player");
+	var PlayerPlaying = GetPlayerCookie("PlayerPlaying");
+	//Player is open
+	if (StreamPlayer) {
+		if (StreamPlayer.closed == false) SetPlayerCookie("Player", 1, 0);
+		else SetPlayerCookie("Player", 0, 0);
+		//Player for reading: No such flag is not used for now but it may be helpful:)
+		if (onPlayFlag) SetPlayerCookie("PlayerPlaying", 1, 0);
+		else SetPlayerCookie("PlayerPlaying", 0, 0);
+	} else {
+		if (Player <= 0) SetPlayerCookie("Player", 0, 0);
+		else SetPlayerCookie("Player", 1, 0);
+		if (PlayerPlaying <= 0) SetPlayerCookie("PlayerPlaying", 0, 0);
+		else SetPlayerCookie("PlayerPlaying", 1, 0);
+	}
+}
+
+function SetPlayerCookie(name, value, expires) {
+	var domain = document.domain;
+	var path = '/';
+	var expDate = new Date();
+	expDate.setTime(expDate.getTime() + (expires * 24 * 3600 * 1000));
+	((path) ? "; path=" + path : "") + ((domain) ? "; domain=" + domain : "");
+}
+/** END **/
+
+
 //on lance la fonction onloadCaller une fois la page chargee.
 $(function(){
     onloadCaller();
